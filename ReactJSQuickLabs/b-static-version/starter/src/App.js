a9b9a5ee9275dbbaf37f4cdcf38e68b6eba1cff4
lamalvin21 <alvinlam21@gmail.com>
2023-08-11 15:50:25 +0100

    adding remote data file

--- ReactJSQuickLabs/b-static-version/starter/src/App.js
+++ ReactJSQuickLabs/b-static-version/starter/src/App.js
@@ -9,29 +9,62 @@ import Footer from './Components/Footer';
 import AllTodos from './Components/AllTodos';
 import AddEditTodo from './Components/AddEditTodo';
 import { useState, useEffect } from 'react';
-import sampleTodos from './sampleTodos.json';
+// import sampleTodos from './sampleTodos.json';
+import axios from 'axios';
+import Modal from './Components/utils/Modal';
+
+const TODOSURL = 'http://localhost:4000/todos';
 
 function App() {
   const [todos, setTodos] = useState({});
+  const [getError, setError] = useState("");
+  const [postError, setPostError] = useState("");
 
   useEffect(() => {
-    setTodos({todos: sampleTodos});
-  });
+    const getData = async () => {
+      setTodos(await getTodos());
+    }
+    setTimeout(() => {
+      getData();
+    }, 5000)
+  }, []);
 
-  const submitTodoInApp = todo => {
+  const getTodos = async () => {
+    try {
+      const response = await axios.get(TODOSURL);
+      return response.data.length ? { todos: response.data } : { error: "No todos found" };
+    } catch (error) {
+      setError('Data not available from server: ${error.message}');
+      return 'Data not available from server: ${error.message}';
+    }
+  };
+
+  const submitTodoInApp = async (todo) => {
+    setPostError("");
+    try {
+      await axios.post(TODOSURL, todo);
+    } catch (error) {
+      setPostError('Error: ${error.message}');
+    } finally {
+      setTodos(await getTodos());
+    }
     const updatedTodos = [...todos, todo];
     setTodos(updatedTodos);
   };
 
   return (
-    <div className="container">
+    <>
+      {getError && <Modal handleClose={()=>setError("")} message={getError}/>}
+      {postError && <Modal handleClose={()=>setPostError(false)} show={postError}/>}
+      <div className="container">
         <Header/>
         <div className='container'>
-          <AllTodos data={{todos}}/>
+          <AllTodos data={todos}/>
           <AddEditTodo submitTodo={submitTodoInApp}/>
         </div>
         <Footer/>
       </div>
+    </>
   );
 }
 
--- ReactJSQuickLabs/b-static-version/starter/src/Components/AllTodos.js
+++ ReactJSQuickLabs/b-static-version/starter/src/Components/AllTodos.js
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './css/AllTodos.css'
 import Todo from './Todo'
 import TodoModel from './utils/Todo.model'
 import propTypes from 'prop-types'
+import { useState } from 'react'
 
-const AllTodos = (data) => {
-    const todos = data.todos?.map(currentTodo => {
-        const todo = new TodoModel(currentTodo.todoDescription, currentTodo.todoDateCreated, currentTodo.todoCompleted, currentTodo._id);
-        return <Todo todo={todo} key={todo._id}  />
-    })
+const AllTodos = ({data}) => {
+    // const todos = data.todos?.map(currentTodo => {
+    //     const todo = new TodoModel(currentTodo.todoDescription, currentTodo.todoDateCreated, currentTodo.todoCompleted, currentTodo._id);
+    //     return <Todo todo={todo} key={todo._id}  />
+    // })
+
+    const [dateStatus, setDateStatus] = useState({name: "loading", message: "Data is loading..."});
+
+    useEffect(() => {
+        if (data?.error) {
+            setDateStatus({name: "error", message: data.error});
+        } else if (data?.error) {
+            const ds = data.todos.length > 0 ? {name: "data", messsage: null} : {name: "nodata", message: "There were no todos previously saved"};
+            setDateStatus(ds);
+        } else {
+            setDateStatus({name: "loading", message: "Data is loading..."});
+        }
+    }, [data]);
+
+    const populateTable = () => {
+        if (data?.todos?.length > 0) {
+            return data.todos.map(currentTodo => {
+                const todo = new TodoModel(currentTodo.todoDescription, currentTodo.todoDateCreated, currentTodo.todoCompleted, currentTodo._id);
+                return <Todo todo={todo} key={todo._id}  />
+            });
+        } else {
+            return <tr><td id={dateStatus.name} colSpan="3">{dateStatus.message}</td></tr>
+        }
+    };
+
   return (
     <div className='row'>
         <h3>Todo List</h3>
@@ -21,7 +47,7 @@ const AllTodos = (data) => {
                 </tr>
             </thead>
             <tbody>
-                {todos}
+                {populateTable()}
             </tbody>
         </table>
     </div>
@@ -29,15 +55,22 @@ const AllTodos = (data) => {
 }
 
 AllTodos.propTypes = {
-    data: propTypes.exact({
-        todos: propTypes.arrayOf(propTypes.exact({
-            todoDescription: propTypes.string,
-            todoDateCreated: propTypes.string,
-            todoCompleted: propTypes.bool,
-            _id: propTypes.string
-        }))
-        }
-    )
+    data: propTypes.oneOfType([
+        propTypes.exact({
+        todos: propTypes.arrayOf(
+            propTypes.exact({
+                todoDescription: propTypes.string,
+                todoDateCreated: propTypes.string,
+                todoCompleted: propTypes.bool,
+                _id: propTypes.string
+            })
+        ),
+        }),
+        propTypes.exact({
+            error: propTypes.string
+        }),
+        propTypes.exact({})
+        ]),
 }
 
 
